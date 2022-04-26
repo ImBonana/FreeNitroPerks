ebb0bbe33144c3ce0cdc8fcdd7261bdb3405eddf
pronoob742 <67657872+pronoob742@users.noreply.github.com>
2022-04-26 21:45:12 +0300

    Update FreeNitroPerks.plugin.js

--- FreeNitroPerks.plugin.js
+++ FreeNitroPerks.plugin.js
@@ -1,8 +1,8 @@
-/**
+**
  * @name FreeNitroPerks
  * @author Im_Banana#6112
  * @description Unlock all screensharing modes, and use cross-server emotes & gif emotes, Discord wide! (You CANNOT upload 100MB files though. :/)
- * @version 1.0.6
+ * @version 1.0.7
  * @authorId 635250116688871425
  * @website https://github.com/pronoob742/FreeNitroPerks
  * @source https://raw.githubusercontent.com/pronoob742/FreeNitroPerks/main/FreeNitroPerks.plugin.js
@@ -39,33 +39,32 @@ module.exports = (() => {
                 "discord_id": "635250116688871425",
                 "github_username": "pronoob742"
             }],
-            "version": "1.0.6",
+            "version": "1.0.7",
             "description": "Unlock all screensharing modes (Not Working *temp*) , and use cross-server emotes & gif emotes, Discord wide! (You CANNOT upload 100MB files though. :/)",
             "github": "https://github.com/pronoob742/FreeNitroPerks",
             "github_raw": "https://raw.githubusercontent.com/pronoob742/FreeNitroPerks/main/FreeNitroPerks.plugin.js"
         },
         "changelog": [
+            {
+                "title": "New Stuff",
+                "items": [
+                    "Add Sticker bypass. Not Working With Animated Stickers"
+                ]
+            },
             // {
-            //     "title": "New Stuff",
+            //     "title": "Bugs Fixes",
+            //     "type": "fixed",
             //     "items": [
-            //         "Added more settings",
-            //         "Added changelog"
+            //         "Fix The Emoji Menu"
             //     ]
             // },
-            {
-                "title": "Bugs Fixes",
-                "type": "fixed",
-                "items": [
-                    "Fix The Emoji Menu"
-                ]
-            },
-            {
-                "title": "Improvements",
-                "type": "improved",
-                "items": [
-                    "Improve The Code"
-                ]
-            },
+            // {
+            //     "title": "Improvements",
+            //     "type": "improved",
+            //     "items": [
+            //         "Improve The Code"
+            //     ]
+            // },
             // {
             //     "title": "On-going",
             //     "type": "progress",
@@ -131,43 +130,30 @@ module.exports = (() => {
             return class NitroPerks extends Plugin {
                 defaultSettings = {
                     "emojiSize": "40",
+                    "stickerSize": "160",
                     "screenSharing": false,
-                    "emojiBypass":true,
+                    "emojiBypass": true,
+                    "sticker": true
                 };
                 settings = PluginUtilities.loadSettings(this.getName(), this.defaultSettings);
                 getSettingsPanel() {
                     return Settings.SettingPanel.build(_ => this.saveAndUpdate(), ...[
-                        new Settings.SettingGroup("Features").append(...[
+                        new Settings.SettingGroup("Screensharing").append(...[
                             new Settings.Switch("High Quality Screensharing", "Enable or disable 1080p/source @ 60fps screensharing. This adapts to your current nitro status.", this.settings.screenSharing, value => this.settings.screenSharing = value)
                         ]),
                         new Settings.SettingGroup("Emojis").append(
                             new Settings.Switch("Nitro Emotes Bypass", "Enable or disable using the Nitro Emote bypass.", this.settings.emojiBypass, value => this.settings.emojiBypass = value),
                             new Settings.Slider("Size", "The size of the emoji in pixels. 40 is recommended.", 16, 64, this.settings.emojiSize, size=>this.settings.emojiSize = size, {markers:[16,20,32,40,64], stickToMarkers:true})
+                        ),
+                        new Settings.SettingGroup("Stickers").append(
+                            new Settings.Switch("Stickers", "Enable or disable using the Nitro Sticker bypass. Not Working With Animated Stickers", this.settings.sticker, value => this.settings.sticker = value),
+                            new Settings.Slider("Size", "The size of the sticker in pixels. 160 is recommended.", 130, 190, this.settings.stickerSize, size=>this.settings.stickerSize = size, {markers:[130,140,150,160,170,180,190], stickToMarkers:true})
                         )
                     ])
                 }
                 
                 saveAndUpdate() {
                     PluginUtilities.saveSettings(this.getName(), this.settings)
-                    // if (this.settings.screenSharing) {
-                    if (false) {
-                        BdApi.injectCSS("screenShare", `#app-mount > div.layerContainer-2v_Sit > div.layer-1Ixpg3 > div > div > form > div:nth-child(2) > div > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(1) > div > button:nth-child(3) {
-                            display: none !important;
-                            }
-                            #app-mount > div.layerContainer-2v_Sit > div.layer-1Ixpg3 > div > div > form > div:nth-child(2) > div > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(1) > div > button:nth-child(3) {
-                            display: none !important;
-                            }
-                            #app-mount > div.layerContainer-2v_Sit > div.layer-1Ixpg3 > div > div > form > div:nth-child(2) > div > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(1) > div > button:nth-child(3) {
-                            display: none !important;
-                            }`)
-                        this.screenShareFix = setInterval(()=>{
-                            document.querySelector("#app-mount > div.layerContainer-2v_Sit > div.layer-1Ixpg3 > div > div > form > div:nth-child(2) > div > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(1) > div > button:nth-child(2)").click()
-                            document.querySelector("#app-mount > div.layerContainer-2v_Sit > div.layer-1Ixpg3 > div > div > form > div:nth-child(2) > div > div > div:nth-child(4) > div > div:nth-child(2) > div:nth-child(1) > div > button:nth-child(2)").click()
-                            clearInterval(this.screenShareFix)
-                        }, 100)
-                    }
-
-                    if (!this.settings.screenSharing) BdApi.clearCSS("screenShare")
 
                     if (this.settings.emojiBypass) {
                         clearInterval(this.fixEmojiMenu)
@@ -187,13 +173,11 @@ module.exports = (() => {
                         // send emoji
                         Patcher.before(DiscordModules.MessageActions, "sendMessage", (_, [, msg]) => {
                             msg.validNonShortcutEmojis.forEach(emoji => {
-                                console.log(emoji)
                                 if (emoji.url.startsWith("/assets/")) return;
                                 msg.content = msg.content.replace(`<${emoji.animated ? "a" : ""}${emoji.allNamesString.replace(/~\d/g, "")}${emoji.id}>`, emoji.url + `&size=${this.settings.emojiSize} `)
                             })
 
                             msg.invalidEmojis.forEach(emoji => {
-                                console.log(emoji)
                                 if (emoji.url.startsWith("/assets/")) return;
                                 msg.content = msg.content.replace(`<${emoji.animated ? "a" : ""}${emoji.allNamesString.replace(/~\d/g, "")}${emoji.id}>`, emoji.url + `&size=${this.settings.emojiSize} `)
                             })
@@ -207,17 +191,61 @@ module.exports = (() => {
                                 obj[2].content = obj[2].content.replace(idfkAnymore, `https://cdn.discordapp.com/emojis/${idfkAnymore.match(/\d{18}/g)[0]}?size=${this.settings.emojiSize}`)
                             })
                         });
-                    }
-
-                    if(!this.settings.emojiBypass) {
-                        Patcher.unpatchAll(DiscordModules.MessageActions)
-                        
+                    } else {
                         document.querySelectorAll(".emojiItem-277VFM.emojiItemMedium-2stgkv").forEach(elem => {
                             elem.onclick = function() { }
                             elem.classList.add("emojiItemDisabled-3VVnwp")
                         })
 
                         clearInterval(this.fixEmojiMenu)
+                    }
+
+                    if(this.settings.sticker) {
+                        clearInterval(this.fixStickerMenu)
+                        this.fixEmojiMenu = setInterval(() => {
+                            let pre = document.querySelector("#sticker-picker-grid ~ .upsellWrapper-3KE9GX")
+                            if(pre != null) pre.remove()
+
+                            document.querySelectorAll(".stickerNode-3aUBs4.stickerUnsendable-PkQAxI").forEach(elem => {
+                                elem.classList.remove("stickerUnsendable-PkQAxI")
+                            })
+
+                            document.querySelectorAll(".sticker-H2HhJD.sticker-2Yb_-L div div div img").forEach(elem => {
+                                elem.onclick = function() {                          
+                                    setTextInTextBox(`$%${elem.dataset.id}%$`)
+                                }
+                            })
+                        }, 100)
+
+                        Patcher.before(DiscordModules.MessageActions, "sendMessage", (_, [, msg]) => {
+                            msg.content.match(/(?<=\$\%)(.*?)(?=\%\$)/g).forEach(id => {
+                                console.log(`$%${id}%$`)
+                                msg.content = msg.content.replace(`$%${id}%$`, `https://media.discordapp.net/stickers/${id}.png?size=${this.settings.stickerSize}`)
+                            })
+                        });
+
+                        //for editing message also
+                        Patcher.before(DiscordModules.MessageActions, "editMessage", (_,obj) => {
+                            let msg = obj[2].content
+                            if (msg.search(/\d{18}/g) == -1) return;
+                            msg.match(/(?<=\$\%)(.*?)(?=\%\$)/g).forEach(id => {
+                                console.log(`$%${id}%$`)
+                                obj[2].content = msg.replace(`$%${id}%$`, `https://media.discordapp.net/stickers/${id}.png?size=${this.settings.stickerSize}`)
+                            })
+                            msg.match(/<a:.+?:\d{18}>|<:.+?:\d{18}>/g).forEach(idfkAnymore=>{
+                                obj[2].content = obj[2].content.replace(idfkAnymore, `https://cdn.discordapp.com/stickers/${idfkAnymore.match(/\d{18}/g)[0]}?size=${this.settings.emojiSize}`)
+                            })
+                        });
+                    } else {
+                        document.querySelectorAll(".stickerNode-3aUBs4").forEach(elem => {
+                            elem.classList.add("stickerUnsendable-PkQAxI")
+                        })
+
+                        document.querySelectorAll(".sticker-H2HhJD.sticker-2Yb_-L div div div img").forEach(elem => {
+                            elem.onclick = function() {                          
+                                
+                            }
+                        })
                     }
                 }
 
@@ -227,8 +255,8 @@ module.exports = (() => {
                 }
 
                 onStop() {
-                    clearInterval(this.screenShareFix)
                     clearInterval(this.fixEmojiMenu)
+                    clearInterval(this.fixStickerMenu)
 
                     BDFDB.PatchUtils.forceAllUpdates(this);
                     Patcher.unpatchAll();
